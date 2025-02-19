f85aea28a503a3103d1d9fe173aee01328596552
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-02-19 17:54:50 +0500

    Variables in python

---
+++ 19-02-2025/index.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("Sohail is my name")
+print("My age is 19")
+
+print("Hello World Sohail is my name" , "My age is 19")
+
+
+print(10)
+print(10 , 20 , 30)
+print(10 + 10)
+print(20 - 10)
+print(5 * 2)
+print(100 / 10)
+print(10 / 2)
+
+# Break Line in python 
+
+print("Hello world\nDo You love python")
+
---
+++ 19-02-2025/variables.py
@@ -0,0 +1,66 @@
+print("Hello Variables")
+
+
+# Create a Variable 
+
+name = "Sohail" # String 
+age = 19  # Integer Value
+price = 25.99 # Float
+
+print(name)
+print(age)
+print(price)
+print("My name is :", name ,  "My age is :", age)
+
+x = 4
+x = "Sally"
+print(x )
+print(x)
+
+# Get the Type
+
+a = str(3)
+print(a)
+print(type(a))
+b = int(-3)
+print(a)
+print(type(b))
+
+
+# 1name
+
+x = 10;
+name = "Sohail"
+price = 99.99;
+
+# Valid Variables
+age = 25;
+_student_name = "Ahmed";
+price100 = 100
+print(age , _student_name , price100)
+
+# Invalid Variables:
+
+# 99price
+# student name Space use nhn kr sakte hai 
+# if = 10
+
+# Mini Project 
+# Simple Calculator 
+
+num1 = float(input("Enter the first number"))
+num2 = float(input("Enter the second number"))
+sum_result = num1 + num2
+sub_result = num1 - num2
+mul_result = num1 * num2
+div_result = num1 / num2 if num2 != 0 else "Infinity"
+
+print("\nResults :")
+print("Addition :" , sum_result)
+print("Subtraction :" ,sub_result)
+print("Multiplication : " , mul_result)
+print("Division :" , div_result)
+
+
+
+
