e7cca9db7ae93a20cbc437f8093687b7783caba2
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-02-28 11:44:39 +0500

    Update file

---
+++ 26-02-2025/first_class.py
@@ -0,0 +1,20 @@
+print("Hello , World ✨🌎")
+print("Hello Sohail")
+print(2 + 2)
+print( 2 * 3)
+
+# Variables
+fname = "Sohail"
+print(fname)
+lname = "Shoukat"
+print(lname)
+age = 19
+print(age)
+
+# Student Data
+studetName = input("Enter the first name : \n")
+studentAge = int(input("Enter the age : \n"))
+studentEmail = input("Enter the email address : \n")
+studentCity = input("Enter the city name : \n")
+print("Student name : " , studetName  ,  "\n"  "Student age :", studentAge , "\n" "Student email : " , studentEmail , "\n" "Student city :"  , studentCity)
+
